2361cafdda5b6b87046eeb788cb0d12bc6d0261b
Herbert Ullrich <ullriher@fel.cvut.cz>
2020-11-25 17:00:57 +0100

    Tutorial

--- views/site/tutorial.php
+++ views/site/tutorial.php
@@ -9,6 +9,8 @@ $this->title = 'Tutoriál';
 
 ?>
 <div class="container">
-    <h1><?=$this->title?></h1>
+    <h1><?=$this->title.($t?' (anotační část)':'')?></h1>
+    <p>Pro osobní rady a konzultace pište Honzovi Drchalovi na <strong>drchajan@fel.cvut.cz</strong> nebo Bertíkovi Ullrichovi na <strong>ullriher@fel.cvut.cz</strong></p>
+    <p><?=Html::a('<i class="fas fa-file-pdf"></i> Slidy (PDF)',['/2020_fcheck_anotace.pdf'], ['class'=>'btn btn-primary'])?></p>
     <iframe src="https://drive.google.com/file/d/1RubaYwDDwrXjUsSTOhLVqcRZXmPLy6nm/preview?t=<?=$t?>" class="w-100" style="height: 40rem"></iframe>
 </div>
