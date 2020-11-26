96dbf5794bb4a7af2710b6cba54820b957e35c9e
Herbert Ullrich <ullriher@fel.cvut.cz>
2020-11-26 14:28:59 +0100

    počty požadovaných anotací

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -50,10 +50,10 @@ AppAsset::register($this);
                     Html::tag('li',
                         Html::tag('span',
                             //'<strong class="fw-500">Ú<sub>0</sub></strong>: ' . Paragraph::find()->where(['candidate_of' => Yii::$app->user->id])->count() .
-                            '<strong class="fw-500">Ú<sub>1</sub>a</strong>: ' . Claim::find()->where(['user' => Yii::$app->user->id])->andWhere(['IS', 'mutation_type', null])->count() .
-                            ', <strong class="fw-500">Ú<sub>1</sub>b</strong>: ' . Claim::find()->where(['user' => Yii::$app->user->id])->andWhere(['IS NOT', 'mutation_type', null])->count() .
-                            ', <strong class="fw-500">Ú<sub>2</sub>a</strong>: ' . Label::find()->where(['user' => Yii::$app->user->id, 'oracle' => true])->count() .
-                            ', <strong class="fw-500">Ú<sub>2</sub>b</strong>: ' . Label::find()->where(['user' => Yii::$app->user->id, 'oracle' => false])->count()
+                            'Ú<sub>1</sub>a<strong class="fw-500">: ' . Claim::find()->where(['user' => Yii::$app->user->id])->andWhere(['IS', 'mutation_type', null])->count() .
+                            '</strong><sub>/5</sub>&nbsp;&nbsp;&nbsp;Ú<sub>1</sub>b: <strong class="fw-500">' . Claim::find()->where(['user' => Yii::$app->user->id])->andWhere(['IS NOT', 'mutation_type', null])->count() .
+                            '</strong><sub>/15</sub>&nbsp;&nbsp;&nbsp;Ú<sub>2</sub>a: <strong class="fw-500">' . Label::find()->where(['user' => Yii::$app->user->id, 'oracle' => true])->count() .
+                            '</strong><sub>/5</sub>&nbsp;&nbsp;&nbsp;Ú<sub>2</sub>b</strong>: <strong class="fw-500">' . Label::find()->where(['user' => Yii::$app->user->id, 'oracle' => false])->count().'</strong><sub>/15</sub>'
                             , ['class' => ' text-black nav-link ']),
                         ['class' => 'text-center nav-item text-black']
                     ),
