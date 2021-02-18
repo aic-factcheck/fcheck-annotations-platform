5db9b0638b8f7aab401e4ef7a8c36571567466d2
Herbert Ullrich <ullriher@fel.cvut.cz>
2021-02-18 09:40:54 +0100

    Annotation cleaning - added timestamps & detokenization

--- views/label/clean.php
+++ views/label/clean.php
@@ -12,24 +12,24 @@ $this->title = 'Protiřečící si anotace';
 ?>
 <div class="container">
     <h1><?= $this->title ?></h1>
-    <?php ActiveForm::begin()?>
-        <p>
-            <?= Html::button('<i class="fas fa-times"></i> Smazat vybrané (měkké smazání)', ['type' => 'submit', 'class' => 'btn btn-danger']) ?>
-        </p>
-        <?php
-        foreach ($conflicts as $conflict) {
-            echo "<h2>" . $conflict[0]->claim0->claim . "</h2>";
-            foreach ($conflict as $label) {
-                echo "<h3><label><input type='checkbox' name='delete[]' value='$label->id'/> $label->label</label></h3> ";
-                foreach ($label->evidences as $evidence) {
-                    echo "<p>" . $evidence->paragraph0->text . "</p>";
-                }
+    <?php ActiveForm::begin() ?>
+    <p>
+        <?= Html::button('<i class="fas fa-times"></i> Smazat vybrané (měkké smazání)', ['type' => 'submit', 'class' => 'btn btn-danger']) ?>
+    </p>
+    <?php
+    foreach ($conflicts as $conflict) {
+        echo "<h2>" . $conflict[0]->claim0->claim . "</h2>";
+        foreach ($conflict as $label) {
+            echo "<h3><label><input type='checkbox' name='delete[]' value='$label->id'/> $label->label</label></h3> ";
+            foreach ($label->evidences as $evidence) {
+                echo "<p>" . ' ' . \app\helpers\Helper::detokenize($evidence->paragraph0->text) . " ".\yii\helpers\Html::tag('small', Yii::$app->formatter->asDatetime($evidence->paragraph0->article0->date), ['class' => 'badge badge-secondary ']) ;
             }
-            echo "<hr/>";
         }
-        ?>
-        <p>
-            <?= Html::button('<i class="fas fa-times"></i> Smazat vybrané (měkké smazání)', ['class' => 'btn btn-danger']) ?>
-        </p>
-    <?php ActiveForm::end()?>
+        echo "<hr/>";
+    }
+    ?>
+    <p>
+        <?= Html::button('<i class="fas fa-times"></i> Smazat vybrané (měkké smazání)', ['class' => 'btn btn-danger']) ?>
+    </p>
+    <?php ActiveForm::end() ?>
 </div>
