29fe9441dcc257d5c53e6f4a4435a03d9e73f6a2
Jan Drchal <drchajan@fel.cvut.cz>
2021-03-08 12:54:29 +0100

    Update index.php - U2

--- views/label/index.php
+++ views/label/index.php
@@ -172,7 +172,7 @@ $this->title = 'Anotace tvrzení';
                         <LI>Správnost výroků dokazujte vždy k datu vydání <em>zdrojového článku</em>:
                             <strong><i class="fas fa-history"></i> <?= Yii::$app->formatter->asDate($model->claim->paragraph0->article0->date) ?></strong>.
                             Tedy relativní určení času ("včera", "letos",...) a v čase pomíjivé jevy ("nejteplejší
-                            léto",...) fact-checkujte z pohledu tohoto dne.
+                            léto",...) ověřujte z pohledu tohoto dne.
                         </LI>
                         <li>Jediným zdrojem informací pro anotaci tvrzení smí být zdrojový článek a texty znalostního
                             rámce. S výjimkou podmíněného potvrzení či vyvrácení (viz níže) <em>je jakékoliv použítí
