9b2c46ab09a37c561d106fe23231dc084bb7de5b
Herbert Ullrich <ullriher@fel.cvut.cz>
2021-05-08 19:58:35 +0200

    chart printing tools

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -146,7 +146,8 @@ AppAsset::register($this);
             Yii::$app->session->remove('mutations');
         } ?>
     </script>
-
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.5/jspdf.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js"></script>
     </body>
     </html>
 <?php $this->endPage() ?>
