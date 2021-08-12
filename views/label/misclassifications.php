44382a71f18035bd8f4da44eda44db8693d81d67
Herbert Ullrich <ullriher@fel.cvut.cz>
2021-08-12 10:00:55 +0200

    misclas view

---
+++ views/label/misclassifications.php
@@ -0,0 +1,34 @@
+<?php
+
+/* @var $this yii\web\View */
+
+/* @var $misclassifications array */
+
+use app\models\Label;
+use yii\bootstrap4\ActiveForm;
+use yii\bootstrap4\Html;
+
+$this->title = 'Protiřečící si anotace';
+?>
+<div class="container">
+    <h1><?= $this->title ?></h1>
+    <?php ActiveForm::begin() ?>
+    <p>
+        <?= Html::button('<i class="fas fa-times"></i>  Uložit úpravy', ['type' => 'submit', 'class' => 'btn btn-primary']) ?>
+    </p>
+    <?php
+    foreach ($misclassifications as $misclassification) {
+        try {
+            echo "<h2>".$misclassification['claim']."</h2>";
+            echo "<p>".json_encode($misclassification)."</p>";
+            echo "<hr/>";
+        } catch (\yii\base\ErrorException $e) {
+            continue;
+        }
+    }
+    ?>
+    <p>
+        <?= Html::button('<i class="fas fa-times"></i>  Uložit úpravy', ['class' => 'btn btn-primary']) ?>
+    </p>
+    <?php ActiveForm::end() ?>
+</div>
