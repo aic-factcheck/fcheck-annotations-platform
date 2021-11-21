6067fd3c5099d82215655e0fdf7d43d1b2debbdb
Herbert Ullrich <ja@bertik.net>
2021-11-21 20:26:16 +0100

    Datasets moved to web-visible dir (temporarily?)

---
+++ controllers/DatasetController.php
@@ -0,0 +1,153 @@
+<?php
+
+namespace app\controllers;
+
+use app\models\Claim;
+use app\models\ClaimKnowledge;
+use app\models\LoginForm;
+use app\models\TimeSpent;
+use app\models\User;
+use Yii;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use yii\httpclient\Client;
+use yii\web\Controller;
+use yii\web\Response;
+
+class SiteController extends Controller
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::class,
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                    [
+                        'actions' => ['login'],
+                        'allow' => true,
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionTutorial($t = 0)
+    {
+        return $this->render('tutorial', ['t' => $t]);
+    }
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionStats()
+    {
+        return $this->render('stats');
+    }
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionStatistics($summer=false)
+    {
+        return $this->render('sandbox',['summer'=>$summer]);
+    }
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionThesis($summer=false)
+    {
+        return $this->render('thesis',['summer'=>$summer]);
+    }
+
+    /**
+     * Login action.
+     *
+     * @return Response|string
+     */
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        if ($model->load(Yii::$app->request->post())) {
+            if ($model->login()) return $this->goBack();
+            Yii::$app->session->addFlash("danger", "Zadané SIDOS ID není v systému");
+        }
+
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Logout action.
+     *
+     * @return Response
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+
+    public function actionTimer($time, $route)
+    {
+        return (new TimeSpent(['user' => Yii::$app->user->id, 'time' => $time, 'route' => $route]))->save();
+    }
+
+    public function actionSite()
+    {
+        return $this->redirect(["/"]);
+    }
+}
