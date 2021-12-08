aa89352931fc340090e426a8c94024760afc7852
Herbert Ullrich <ja@bertik.net>
2021-12-08 17:40:54 +0100

    Winter semester dashboard update

--- views/site/sandbox.php
+++ views/site/sandbox.php
@@ -13,7 +13,7 @@ use yii\helpers\Url;
 use yii\web\JsExpression;
 
 $this->title = 'Statistiky';
-$summerStamp = strtotime('2021-03-01');
+$summerStamp = strtotime('2021-12-01');
 $beginStamp = $summer ? strtotime('2021-03-01') : strtotime('2020-11-01');
 $display = ['>=', 'created_at', $beginStamp];
 $winterOnly = "WHERE `created_at`<$summerStamp";
@@ -184,7 +184,7 @@ ksort($evidence_pars)
     </div>
     <p>
         <a href="?summer=<?= !$summer ? 1 : 0 ?>" class="btn btn-default">
-            <i class="fas fa-toggle-<?= $summer ? 'on' : 'off' ?>"></i> Zobrazit pouze letní semestr
+            <i class="fas fa-toggle-<?= $summer ? 'on' : 'off' ?>"></i> Zobrazit pouze aktuální semestr (ZS'21/'22)
         </a></p>
     <div class="row">
         <div class="col-lg-4">
